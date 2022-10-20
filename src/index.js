f61f8df37b70b1a61db3b34c44bd3e731ccb53dc
AlexKovalchuk <kovalchuk111@gmail.com>
2022-10-20 23:47:49 +0300

    HW#9 js_css_using selectors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".detail-image");
 const detailsTitle = document.querySelector(".detail-title");
+const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden";  //assignment the classes name "hidden" to variable HIDDEN
+
 function setDetails(anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
+    showDetails();
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");    
 }
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         setDetails(anchors[i])
     });
+}
+
+function showDetails() {
+    mainClass.classList.remove(HIDDEN);
+}
+
+function hideDetails() {
+    mainClass.classList.add(HIDDEN);
 }
