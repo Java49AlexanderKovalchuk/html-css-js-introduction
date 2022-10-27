e1d5903c0c026dc8919019f7caf56d9a69ec6c32
AlexKovalchuk <kovalchuk111@gmail.com>
2022-10-27 10:54:58 +0300

    HW#11 add if case negative number

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 
 function sumDigits(number) {
+    if(number < 0) {
+        number = -number;
+    }
     let sum = 0;
-    for (let i = number; i > 0; i = Math.floor(i / 10)) {
+    for (let i = number; i > 0; i = Math.trunc(i / 10)) {
         console.log("i = ", i);  // see "i" in each iteration
         number = i % 10;         // get the last digit of the number in the iteration
         sum = sum + number;      
@@ -10,4 +13,4 @@ function sumDigits(number) {
 }
 
 // let's test
-console.log(sumDigits(1020305));
+console.log(sumDigits(-1020305));
