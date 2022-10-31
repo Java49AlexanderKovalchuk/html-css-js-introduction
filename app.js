969ab064658935c0fe07dae374c91b0b8ff7e870
AlexKovalchuk <kovalchuk111@gmail.com>
2022-11-01 00:11:09 +0200

    HW#12 coversions with number systems

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
 
-function sumDigits(number) {
-    if (number < 0) {
-        number = -number;
+function fromNumberToString(number, base) {
+    if (base < 2 || base > 36) {
+        console.log("ERROR! Please take the base in the interval [2, 36]");
+        return -1;
     }
-    let sum = 0;
-    while (number > 0) {
-        sum += number % 10;
-        number = Math.trunc(number / 10);
-    }
-    return sum;
+    number = Math.abs(number);
+    let res = '';
+    let symbol;
+    do {
+        let digit = number % base;
+        if (digit <= 9) {
+            symbol = String.fromCharCode(48 + digit);
+        }
+        else {
+            symbol = String.fromCharCode(87 + digit);
+        }
+        res = symbol + res;
+        number = Math.trunc(number / base);
+    } while (number != 0);
+    return res;
 }
 
-// let's test
-console.log(sumDigits(-1020305));
+function fromStringToNumber(code, base) {
+    let res = 0;
+    let rate;
+    let calibration;
+    for (let i = 0; i < code.length; i++) {
+        let numChar = code[i].charCodeAt();
+        if (numChar < 48 || (numChar > 57 && numChar < 97) || numChar > 122) {
+            console.log('ERROR! Please use in the code digits [0, 9] and undercase letters only. For example: 3w0a');
+            return -1;
+        }
+        if (isNaN(code[i])) {
+            calibration = "a".charCodeAt();
+            rate = numChar - calibration + 10;
+        }
+        else {
+            calibration = "0".charCodeAt();
+            rate = numChar - calibration;
+        }
+        res = res * base + rate;
+    }
+    return res;
+}
+
+//TEST
+console.log(fromNumberToString(900550, 36));
+console.log(fromNumberToString(46016237, 36));
+console.log(fromNumberToString(11483, 2));
+
+console.log(fromStringToNumber("java", 36));
+console.log(fromStringToNumber("react", 36));
+console.log(fromStringToNumber("10110011011011", 2));
