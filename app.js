21d84bba672dcd059ae0ff38bad14dfa71b9a051
AlexKovalchuk <kovalchuk111@gmail.com>
2022-11-04 02:28:25 +0200

    HW#13 arrays methods, control TZ

--- app.js
+++ app.js
@@ -1,54 +1,61 @@
 
-function fromNumberToString(number, base) {
-    if (base < 2 || base > 36) {
-        console.log("ERROR! Please take the base in the interval [2, 36]");
+function checkTeudatZehut(teudatStrNumber) {
+    if (!validationTeudaIsString(teudatStrNumber)) {        // check the argument
         return -1;
     }
-    number = Math.abs(number);
-    let res = '';
-    let symbol;
-    do {
-        let digit = number % base;
-        if (digit <= 9) {
-            symbol = String.fromCharCode(48 + digit);
+    let arElemStr = Array.from(teudatStrNumber);            //get array of string elements
+    let sumDigits = sumElementsByAlgorithm(arElemStr);
+    return sumDigits % 10 == 0 ? true : false;
+}
+function ifElemTwoDig(el) {
+    return el % 10 + Math.trunc(el / 10);
+}
+function sumElementsByAlgorithm(ar) {
+    return ar.reduce(function (res, cur, i) {
+        if (i % 2 == 0) {
+            return res = res + (+cur);
         }
-        else {
-            symbol = String.fromCharCode(87 + digit);
-        }
-        res = symbol + res;
-        number = Math.trunc(number / base);
-    } while (number != 0);
+        cur *= 2;
+        return cur <= 9 ? res + cur : res + ifElemTwoDig(cur);
+    }, 0);
+}
+
+function generateRandomTeudatZehut() {
+    let arRandom8Digits = [];
+    for (let i = 0; i < 8; i++) {
+        arRandom8Digits[i] = Math.round(Math.random() * 9);
+    }
+    let sum8Rand = sumElementsByAlgorithm(arRandom8Digits);
+    let controlDigit = sum8Rand % 10 == 0 ? 0 : 10 - sum8Rand % 10;
+    arRandom8Digits[8] = controlDigit;   //add 9-th digit like a control digit to array
+    let teudatZehutStr = arToString(arRandom8Digits);
+    return teudatZehutStr;
+}
+function arToString(ar) {
+    res = '';
+    ar.forEach(function (el) {
+        res = res + el;
+    })
     return res;
 }
 
-function fromStringToNumber(code, base) {
-    let res = 0;
-    let rate;
-    let calibration;
-    for (let i = 0; i < code.length; i++) {
-        let numChar = code[i].charCodeAt();
-        if (numChar < 48 || (numChar > 57 && numChar < 97) || numChar > 122) {
-            console.log('ERROR! Please use in the code digits [0, 9] and undercase letters only. For example: 3w0a');
-            return -1;
-        }
-        if (isNaN(code[i])) {
-            calibration = "a".charCodeAt();
-            rate = numChar - calibration + 10;
-        }
-        else {
-            calibration = "0".charCodeAt();
-            rate = numChar - calibration;
-        }
-        res = res * base + rate;
+function validationTeudaIsString(teudatStrNumber) {
+    if (typeof teudatStrNumber != 'string') {
+        console.log('ERROR! TZ should be a string')
+        return false;
     }
-    return res;
+    for (let i = 0; i < Array.from(teudatStrNumber).length; i++) {
+        if (isNaN(Array.from(teudatStrNumber)[i])) {
+            console.log('ERROR! The TZ should be consist from digits only');
+            return false;
+        }
+    }
+    return true;
 }
 
 //TEST
-console.log(fromNumberToString(900550, 36));
-console.log(fromNumberToString(46016237, 36));
-console.log(fromNumberToString(11483, 2));
-
-console.log(fromStringToNumber("java", 36));
-console.log(fromStringToNumber("react", 36));
-console.log(fromStringToNumber("10110011011011", 2));
+console.log("Random number of TZ is", generateRandomTeudatZehut());
+console.log(typeof generateRandomTeudatZehut());
+console.log(checkTeudatZehut(generateRandomTeudatZehut()));
+console.log(checkTeudatZehut('123e43212'));
+console.log(checkTeudatZehut(123456789));
