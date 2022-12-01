e9a67751483b1eef6aafa5f2a7cbb12b6572a9dc
AlexKovalchuk <kovalchuk111@gmail.com>
2022-12-02 00:50:31 +0200

    HW-21 form - improved

--- src/form.js
+++ src/form.js
@@ -1,15 +1,17 @@
 const COLOR_MESSAGE = 'red';
-const messageSalary = "Salary should be in the range [1000, 40000]";
+const MES_SALARY = "Salary should be in the range [1000, 40000]";
 let currentYear = new Date().getFullYear();
-const messageYear = `Birthyear shouldn't be less then 1950 and greater then ${currentYear}`;
+const MES_YEAR = `Birthyear shouldn't be less then 1950 and greater then ${currentYear}`;
+
+const errSalary = MES_SALARY;
+const errYear = MES_YEAR;
+const colorMessage = COLOR_MESSAGE;
 
 const inputElements = document.querySelectorAll(".form-class [name]");
-const message = document.querySelector(".message");
+const messageElement = document.querySelector(".message");
 
 function onSubmit(event) {
     event.preventDefault();
-    console.log("submitted");
-
     const employee = Array.from(inputElements).reduce(
         (res, cur) => {
             res[cur.name] = cur.value;
@@ -17,43 +19,39 @@ function onSubmit(event) {
         }, {}
     );
     console.log(employee);
-    //console.log(employee.birthDate);
-
 }
 
-
-function onChange(event) {
-    
+function onChangeSalary(event) {
     if (event.target.name = "salary") {
         console.log('salary: ', event.target.value);
         if (+event.target.value < 1000 || +event.target.value > 40000) {
             event.target.value = '';
-            message.hidden = false;
-            getMessage(messageSalary);
-            setTimeout(disappearMessage, 5000);
+            outPutError(errSalary);
         }
     }
-
 }
-function onChangeBirth(event) {
-    
-    if(event.target.name = "birthDate") {
+
+function onChangeYear(event) {
+    if (event.target.name = "birthDate") {
         let chosenYear = +event.target.value.slice(0, 4);
-        console.log('birthYear: ',chosenYear);
-        if(chosenYear < 1950 || chosenYear > currentYear) {
+        console.log('birthYear: ', chosenYear);
+        if (chosenYear < 1950 || chosenYear > currentYear) {
             event.target.value = '';
-            message.hidden = false;
-            getMessage(messageYear);
-            setTimeout(disappearMessage, 5000);
+            outPutError(errYear);
         }
     }
-
 }
 
 function getMessage(textMessage) {
-    message.innerHTML = textMessage;
-    message.style.color = COLOR_MESSAGE;
+    messageElement.innerHTML = textMessage;
+    messageElement.style.color = colorMessage;
 }
 function disappearMessage() {
-    message.hidden = true;
+    messageElement.hidden = true;
+}
+
+function outPutError(mes) {
+    messageElement.hidden = false;
+    getMessage(mes);
+    setTimeout(disappearMessage, 3000);
 }
